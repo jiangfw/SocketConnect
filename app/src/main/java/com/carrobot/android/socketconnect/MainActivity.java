a8d9b2b4be427effb5bea651c6704cc45e7c2245
fuwei.jiang <fuwei.jiang@carrobot.com>
2017-02-20 20:08:51 +0800

    second pull

--- app/src/main/java/com/carrobot/android/socketconnect/MainActivity.java
+++ app/src/main/java/com/carrobot/android/socketconnect/MainActivity.java
@@ -184,6 +184,6 @@ public class MainActivity extends AppCompatActivity implements onSocketStatusLis
         super.onDestroy();
         mSocketManager.stopSocketConnection();
         mSocketManager.removeOnSocketStatusListener(this);
-        //TODO test
+        //TODO test 2
     }
 }
